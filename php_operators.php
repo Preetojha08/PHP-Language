8d1e057b7d9594592bfd6030f1c4e91e9f6c571d
Preet Ojha <ozapreet08@gmail.com>
2021-07-21 17:25:54 +0530

    first commit

--- php_operators.php
+++ php_operators.php
@@ -7,7 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PHP Program </title>
 </head>
-        <h1>Operators in PHP</h1>
+        <h1>Operators on PHP</h1>
 <body>
 
     <?php
